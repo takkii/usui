d20350ce84707f049672e3077f4eaa9b9ffe3e10
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-09-29 16:14:17 +0900

    Update.

--- lib/usui/version.rb
+++ lib/usui/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Usui
-  VERSION = '0.0.6.3'.freeze
+  VERSION = '0.0.6.2'.freeze
 end
 
 # 0.0.1→hello,world
@@ -12,7 +12,6 @@ end
 # 0.0.5→18ファイルまでzip圧縮可能
 # 0.0.6→RubyGemsに表示するファイルを1file→18fileに変更
 # 0.0.6.1→文字列にutf8指定
-# 0.0.6.2→内部デバッグで修正
-# 0.0.6.3→RubyGems側ホームページリンク切れ対応
+# 0.0.6.2→内部デバッグで修正、RubyGems側ホームページリンク切れ対応
 
 __END__
